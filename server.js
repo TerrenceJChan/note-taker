964cf514eae3b06b305fa9f7c14e9c4e82181368
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-20 15:15:43 -0500

    Add general server porting code to use with express.

---
+++ server.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Middleware for push and put requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Front-facing front-end folder path.
+app.use(express.static('./public'));
+
+// Routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Server porting.
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
