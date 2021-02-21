dda3bdb8f8dd6a1948cd49645b911ccbab082c79
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-21 12:55:58 -0500

    Add proper routing to add new note entries.

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
+const unid = require('uniqid');
+const fs = require('fs');
+const path = require('path');
 const notes = require('../../db/db.json');
-const { filterByQuery, createNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 })
+
+router.post('/notes', (req, res) => {
+    let newNote = req.body;
+    newNote.id = unid();
+    notes.push(newNote);
+
+    fs.writeFile("db/db.json", JSON.stringify(notes), err => {
+        if (err) {
+            throw err;
+        } else {
+            return true;
+        }
+    });
+
+    res.json(notes);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 
 const express = require('express');
-const fs = require('fs');
-const path = require("path");
 
 const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
-
-// Middleware for push and put requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Front-facing front-end folder path.
+// Front-end folder path.
 app.use(express.static('./public'));
 
 // Routes
 app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 // Server porting.
 app.listen(PORT, () => {
