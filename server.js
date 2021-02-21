1496a6cf0e6dd1b177bc1e587225f5cbad074eb3
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-20 19:12:54 -0500

    Server loads HTML.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 
 const express = require('express');
+const fs = require('fs');
+const path = require("path");
 
-const apiRoutes = require('./routes/apiRoutes');
+// const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // Middleware for push and put requests
@@ -12,13 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Front-facing front-end folder path.
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // Routes
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Server porting.
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-});
+}); 
