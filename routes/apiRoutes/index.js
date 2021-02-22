1dca189343ecfbc4fbb359637a4b50c4fcf758c2
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-21 21:05:17 -0500

    Update README.md file. Add comments to code.

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 const path = require('path');
 let notes = require('../../db/db.json');
 
+// Get request to populate the UI with information from the db.json file.
 router.get('/notes', (req, res) => {
     res.json(notes);
 })
 
+// Saves a new note with a unique ID thanks to the uniqid package. It pushes the new note as an object into the db.json array.
 router.post('/notes', (req, res) => {
     let newNote = req.body;
     newNote.id = unid();
@@ -24,6 +26,7 @@ router.post('/notes', (req, res) => {
     res.json(notes);
 });
 
+// Filters for the ID associated with the note to be deleted. Creates a new array without it, and saves it into the db.json file.
 router.delete('/notes/:id', (req, res) => {
     let result = notes.filter(note => note.id !== req.params.id);
 
